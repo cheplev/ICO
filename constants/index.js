e9eeceb3ec86409446329bf84fe1ae547dbf2000
chep <vcheplev42@gmail.com>
2023-01-16 18:12:07 +0400

    first commit

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xB5f8D4f4e32D01dAD93932747d9F03263e444EfB";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
